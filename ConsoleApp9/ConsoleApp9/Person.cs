44603bb72ebeae1f90a1bcae5cf7af418d4fd585
amity94 <45635119+amity94@users.noreply.github.com>
2019-02-24 20:22:09 +0200

    Add files via upload

---
+++ ConsoleApp9/ConsoleApp9/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    public class Person
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public double Height { get;  set; }
+        protected string Adress { get; set; }
+        private int Code { get; set; }
+        internal int Cars { get; set; }
+
+
+        public Person()
+        {
+
+        }
+
+        public Person(string firstname, string lastname, double height, string adress, int code, int cars)
+        {
+            this.Firstname = firstname;
+            this.Lastname = lastname;
+            this.Height = height;
+            this.Adress = adress;
+            this.Code = code;
+            this.Cars = cars;
+        }
+
+        public override string ToString()
+        {
+            return $"name: {Firstname + " " + Lastname}, height: {Height}, adress: {Adress}, code: {Code}, cars: {Cars}";
+        }
+
+    }
+}
---
+++ ConsoleApp9/ConsoleApp9/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person("amity","nissimov", 1.83, "maginim 6 street", 44, 5);
+            Console.WriteLine(p1);
+
+            XmlSerializer myXmlSerializer = new XmlSerializer(typeof(Person));
+
+            using (Stream file = new FileStream(@"C:\temp\xmlfile.xml", FileMode.Create))
+            {
+                myXmlSerializer.Serialize(file, p1);
+            }
+
+            using (Stream file = new FileStream(@"C:\temp\xmlfile.xml", FileMode.Open))
+            {
+                p1 = myXmlSerializer.Deserialize(file) as Person;
+            }
+
+            Console.WriteLine(p1);
+        }
+    }
+}
